360dec4b284423b6804ad2df36a548af30b12de0
hagyhang <hagyhang@gmail.com>
2018-05-28 10:23:40 +0700

    copied

---
+++ js/datetime.js
@@ -0,0 +1,14 @@
+/*-- options datetimepicker --*/
+$(".form_date").datetimepicker({
+    //language:  'fr',
+    // format: 'yyyy-mm-dd hh:ii',
+    format: 'dd-mm-yyyy',
+    weekStart: 1,
+    todayBtn:  1,
+    autoclose: 1,
+    todayHighlight: 1,
+    startView: 2,
+    minView: 2,
+    forceParse: 0
+});
+/*-- end options datetimepicker --*/
